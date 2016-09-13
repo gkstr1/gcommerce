eba7c7fad065e38b017d87b7a1130534211ff6c8
gkstr1 <gkstr1@hotmail.com>
2016-09-13 23:42:39 +0000

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :sneakers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  root 'sneakers#index'
+ end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
+<<<<<<<<< saved version
 require 'csv'
 CSV.foreach(Rails.root.join("db/seeds_data/sneakers.csv"), headers: true) do |row|
   Movie.find_or_create_by(title: row[0], release_year: row[1], price: row[2], description: row[3], color: row[4], size: row[5])
 end
+=========
+
+>>>>>>>>> local version
